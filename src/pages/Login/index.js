2a6fb4af98aebe347d202d3b060e4d23afe3453c
Edson de Oliveira Correia <118702317+jovem-edson@users.noreply.github.com>
2024-10-29 20:29:31 +0000

    criando tela de login

---
+++ src/pages/Login/index.js
@@ -0,0 +1,40 @@
+import './index.scss'
+
+import Cabecalho from '../../components/cabecalho'
+
+export default function Home() {
+    return (
+        <div className='pagina-login'>
+            <Cabecalho />
+            <section className='primeira-secao-login'>
+            <div className="primeira-secao-login-container">
+    <div className="quadro-boas-vindas">
+        <h1>Bem-vindo, Colaborador!</h1>
+        <img src="/assets/images/pessoas/colaboradora.svg" alt="Colaboradora" />
+    </div>
+
+    <div className="quadro-login">
+        <form className="formulario">
+            <h2>Login</h2>
+            <div className="campo-formulario-nomes">
+                <div className="campo-formulario">
+                    <input type="text" name="nome" placeholder='Insira o usuário de rede...' />
+                </div>
+
+                <div className="campo-formulario">
+                    <input type="password" name="senha" placeholder='Insira sua senha...' />
+                </div>
+            </div>
+            <div>
+                <input type="submit" value="Enviar" />
+            </div>
+        </form>
+    </div>
+</div>
+            </section>
+
+        </div>
+
+    )
+
+}
